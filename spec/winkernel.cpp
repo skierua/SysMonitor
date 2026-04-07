aed9f9ae4d728d58837b561d9a86ccdcbe1a2559
skierua <iurii.vasurchak@gmail.com>
2026-04-07 18:07:51 +0300

    add win support

---
+++ spec/winkernel.cpp
@@ -0,0 +1,217 @@
+#include <windows.h>
+#include <inttypes.h>
+#include <tlhelp32.h>
+#include <minwinbase.h>
+#include <iomanip>
+// #include <cstdlib>
+// #include <iomanip>
+#include <psapi.h>
+#include <tchar.h>
+
+#include "WinKernel.h"
+
+// WinKernel::WinKernel() {}
+
+int WinKernel::canTerminate(int pid) {
+    auto pr = ProcHandle(static_cast<DWORD>(pid), PROCESS_TERMINATE);
+    if (!pr.isValid()) return -1;
+    return 0;
+}
+
+int WinKernel::termProc(int pid) {
+    // Gracefully close GUI process
+       auto closeGui = [](DWORD lpid) {
+           struct EnumData {
+            DWORD pid;
+            HWND hwnd;
+           };
+           EnumData data = { lpid, NULL };
+           EnumWindows([](HWND crnt_hwnd, LPARAM lParam) -> BOOL {
+               EnumData* data = reinterpret_cast<EnumData*>(lParam);
+               DWORD crntPID{0};
+               GetWindowThreadProcessId(crnt_hwnd, &crntPID);
+               // std::cout << "closeGui crntPID=" << crntPID << " lParam=" << *(reinterpret_cast<DWORD*>(lParam)) << std::endl;
+
+               if (crntPID == data->pid && GetWindow(crnt_hwnd, GW_OWNER) == nullptr ) {
+                   if (IsWindowVisible(crnt_hwnd) && GetWindowTextLength(crnt_hwnd) > 0 && GetParent(crnt_hwnd) == NULL){
+                       data->hwnd = crnt_hwnd;
+                       return false;
+                   }
+               }
+               return TRUE;
+           }, reinterpret_cast<LPARAM>(&data));
+           return PostMessage(data.hwnd, WM_CLOSE, 0, 0) != 0;
+       };
+
+
+       // Gracefully close console process
+       auto closeConsole = [](DWORD lpid) {
+           // Attach to the console of the target process
+           if (!AttachConsole(lpid)) {
+               return false;
+           }
+           // Disable Ctrl+C handling for our own process
+           SetConsoleCtrlHandler(nullptr, TRUE);
+
+           bool res = GenerateConsoleCtrlEvent(CTRL_C_EVENT, 0) != 0;
+
+           // Detach from console
+           FreeConsole();
+           // Re-enable Ctrl+C handling
+           SetConsoleCtrlHandler(nullptr, FALSE);
+
+
+           return res;
+       };
+
+       bool success = false;
+
+       // Try GUI close first
+       if (closeGui(static_cast<DWORD>(pid))) {
+           success = true;
+       }
+       // Try console close if GUI close failed
+       else if (closeConsole(static_cast<DWORD>(pid))) {
+           success = true;
+       }
+
+       if (!success) {
+           // std::cerr << "Could not gracefully terminate process " << pid
+           //           << ". It may not have a window or console.\n";
+           return -1;
+       }
+
+       return 0;
+}
+
+VProcInfoList WinKernel::procList() {
+    // std::cout << "WinLib getProc" << std::endl;
+    VProcInfoList res;
+
+    DWORD dwPriorityClass;
+
+    // Take a snapshot of all processes in the system.
+    auto procSnap = SnapHandle(TH32CS_SNAPPROCESS);
+    if( !procSnap.isValid() )
+    {
+        // std::cerr << "CreateToolhelp32Snapshot (of processes)" << std::endl;
+        return std::move(res);
+    }
+
+    // Set the structure and size
+    PROCESSENTRY32 pe32;
+    pe32.dwSize = sizeof( PROCESSENTRY32 );
+
+    // Retrieve information about the first process,
+    if( !Process32First( procSnap.get(), &pe32 ) )
+    {
+        return std::move(res);
+    }
+
+
+    // get information about each process
+    HANDLE hProcess;
+    PROCESS_MEMORY_COUNTERS mem;
+    FILETIME creationTime, exitTime, kernelTime, userTime;
+    do
+    {
+        vk_proc_info vpri;
+
+        // Retrieve the priority class.
+        dwPriorityClass = 0;
+        auto proc = ProcHandle(pe32.th32ProcessID, PROCESS_ALL_ACCESS);
+        if( !proc.isValid() ){
+            continue;
+        } else {    // not actualy used
+            dwPriorityClass = GetPriorityClass( proc.get() );
+            if( !dwPriorityClass ){
+                // std::cout << "GetPriorityClass" << std::endl;
+                continue;
+            }
+        }
+
+        if (GetProcessMemoryInfo(proc.get(), &mem, sizeof(mem))) {
+            // std::cout << "Process ID: " << processID << std::endl;
+            // std::cout << "Working Set Size: " << mem.WorkingSetSize / 1024 << " KB" << std::endl;
+            // std::cout << "Pagefile Usage: " << pmc.PagefileUsage / 1024 << " KB" << std::endl;
+        }   // else std::cout << "Working Set Size: ERROR" << std::endl;
+
+        ULARGE_INTEGER uliTime;
+        uliTime.LowPart  = 0;
+        uliTime.HighPart = 0;
+        if (!GetProcessTimes(proc.get(), &creationTime, &exitTime, &kernelTime, &userTime)) {
+            // std::cerr << "GetProcessTimes failed. Error code: " << GetLastError() << "\n";
+            // return 1;
+        } else {
+            uliTime.LowPart  = creationTime.dwLowDateTime;
+            uliTime.HighPart = creationTime.dwHighDateTime;
+        }
+        vpri.pid = pe32.th32ProcessID;
+        vpri.ppid = pe32.th32ParentProcessID;
+        // vpri.comm = std::wstring(buffer.data()); //pe32.szExeFile;
+        vpri.qcomm = QString(pe32.szExeFile);
+        vpri.mem = mem.WorkingSetSize / 1024;
+        vpri.vm = 0;
+        vpri.th_all = pe32.cntThreads;
+        vpri.th_active = 0;
+        vpri.tm = (uliTime.QuadPart / WIN_TICK_COEF) - WIN_EPOC_DIFF;
+        vpri.uid = 0;
+        res << vpri;
+
+    } while( Process32Next( procSnap.get(), &pe32 ) );
+
+    return std::move(res);
+}
+
+QString WinKernel::procPath(int pid) {
+    auto res = QString("");
+    auto proc = ProcHandle(static_cast<DWORD>(pid), PROCESS_QUERY_LIMITED_INFORMATION);
+
+    if (proc.isValid()) {
+        TCHAR filePath[MAX_PATH];
+        DWORD size = MAX_PATH;
+        if (QueryFullProcessImageName(proc.get(), 0, filePath, &size) > 0) {
+            // std::wcout << L"Process Path: " << filePath << std::endl;
+            res = QString(filePath);
+        } else {
+            // std::cerr << "Failed to retrieve process path." << std::endl;
+        }
+    } else {
+        // std::cerr << "Failed to open process." << std::endl;
+    }
+    return res;
+}
+
+// res =0 for error, but it's not fair enought
+uint64_t WinKernel::sizeRAM() {
+    uint64_t res{0};  // same as unsigned long long
+
+    // Retrieve the amount of physically installed RAM in kilobytes
+    if (GetPhysicallyInstalledSystemMemory(&res)) {
+        // std::cout << "Total Installed RAM: " << res << " GB" << std::endl;
+    } else {
+        // std::cerr << "Error: Unable to retrieve RAM size. Error code: "
+        //           << GetLastError() << std::endl;
+        return 0;
+    }
+
+    return res * 1024;
+}
+
+// res =0 for error, but it's not fair enought
+uint64_t WinKernel::usageRAM() {
+    uint64_t res{0};  // same as unsigned long long
+    MEMORYSTATUSEX memInfo;
+    memInfo.dwLength = sizeof(memInfo);
+
+    if (!GlobalMemoryStatusEx(&memInfo)) {
+        return res; // res =0, API call failed
+    }
+
+    SIZE_T totalPhys = memInfo.ullTotalPhys; // Total physical memory
+    SIZE_T freePhys  = memInfo.ullAvailPhys; // Available physical memory
+    res = totalPhys - freePhys;        // Used memory
+
+    // std::cout << "RAMu: " << res << std::endl;
+    return res;
+}
---
+++ spec/winkernel.h
@@ -0,0 +1,90 @@
+#ifndef WINKERNEL_H
+#define WINKERNEL_H
+
+// #include <iostream>
+// #include <string>
+#include <vector>
+#include <QString>
+
+#include "../shared/TempLib.h"
+
+using VProcInfoList = QList<vk_proc_info>;
+
+#ifndef WIN_TICK_COEF   // actialy nano
+#define WIN_TICK_COEF 10000000ULL
+#endif
+#ifndef WIN_EPOC_DIFF
+#define WIN_EPOC_DIFF 11644473600ULL
+#endif
+
+#include "../shared/TempLib.h"
+
+class ProcHandle {
+
+    HANDLE m_procHandle;
+public:
+    explicit ProcHandle(DWORD pid, DWORD access_flag = PROCESS_ALL_ACCESS) {
+        m_procHandle =OpenProcess(access_flag, FALSE, pid);
+    }
+
+    ~ProcHandle() noexcept {
+        if( isValid() ) CloseHandle(m_procHandle);
+    }
+
+    HANDLE& get() { return m_procHandle; }
+
+    bool isValid() const {
+        return m_procHandle != NULL;
+    }
+};
+
+class SnapHandle {
+
+    HANDLE m_snapHandle;
+public:
+    explicit SnapHandle(DWORD flag = TH32CS_SNAPPROCESS) {
+        m_snapHandle = CreateToolhelp32Snapshot( flag, 0 );
+    }
+
+    ~SnapHandle() noexcept {
+        if( isValid() ) CloseHandle(m_snapHandle);
+    }
+
+    HANDLE& get() { return m_snapHandle; }
+
+    bool isValid() const {
+        return m_snapHandle != INVALID_HANDLE_VALUE;
+    }
+};
+
+class WinKernel : public StaticBase<WinKernel>
+{
+    friend class StaticBase<WinKernel>;
+
+public:
+    static WinKernel & getSelf() {
+        static WinKernel self;
+        return self;
+    }
+    ~WinKernel() noexcept = default;
+
+    int test() {return 42;}
+    int crntEUID() { return 0; }
+    int canTerminate(int pid);
+    int termProc(int pid);
+    VProcInfoList procList();
+    QString procPath(int pid);
+    uint64_t sizeRAM();
+    uint64_t usageRAM();
+    const QString& lastError() { return m_lastError; }
+    void setLogPath(const QString& path) { m_logPath = path; }
+    const QString& logPath() { return m_logPath; }
+
+private:
+    static WinKernel * self;
+
+    QString m_lastError{QString("")};
+    QString m_logPath{QString("./Logs")};
+};
+
+#endif // WINKERNEL_H
